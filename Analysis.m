70df63e20f43ed71fb46619c53f3ceb770819385
himanshu <himanshu14121992@gmail.com>
2015-03-09 18:10:51 +0530

    handled i using numel(i)~=0 and corrections made

--- Analysis.m
+++ Analysis.m
@@ -3,26 +3,32 @@
 % Reading 
 listing = dir('*.dat');
 nGroups = 10;
-nN = 100;
+nN = 20;
 l_lst = length(listing);
 size(listing)
 for q=1:l_lst
     m = csvread(listing(q).name);
     spikes = m(:,2+2*nGroups*nN:201+2*nGroups*nN);
-  
+    imagesc(spikes)
     %PSTH
     %figure;
-    psth = zeros(nGroups,length(m(:,1)));
+    psth = zeros(length(spikes(:,1)),nGroups);
     for g = 1:nGroups
-    %    subplot(nGroups,1,g);
-        psth(:,g) = sum(:,spikes((g-1)*nN+1:g*nN));
-    %    plot(psth(g,:));
+        subplot(nGroups,1,g);
+        %imagesc(sum(spikes(:,(g-1)*nN+1:g*nN)'))
+        psth(:,g) = sum(spikes(:,(g-1)*nN+1:g*nN)');
+        plot(psth(:,g));
         [i,j,k]=find(psth(:,g)>=1);
-        va(g)=var(psth(i(1):i(length(i)),g));
-        mn(g)=mean(psth(i(1):i(length(i)),g));
+        if numel(i)~=0
+            va(g)=var(psth(i(1):i(length(i)),g));
+            mn(g)=mean(psth(i(1):i(length(i)),g));
+        else
+            va(g) = 0;
+            mn(g) = 0;
+        end
     end
-    %print('-dpng',sprintf('PSTH %s.png',listing(q).name));
-    %close();
+    print('-dpng',sprintf('PSTH %s.png',listing(q).name));
+    close();
     
     %fano factor calculation
     ff=va./mn;

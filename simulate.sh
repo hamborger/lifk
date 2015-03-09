68cc3965ca2b2aea39e423f590fb826d8182cef5
himanshu <himanshu14121992@gmail.com>
2015-03-09 14:30:47 +0530

    make compile removed run.sh clone

---
+++ simulate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "***Generate config files nsets.conf and ssets.conf***"
+g++ -std=c++11 ConnGen.cpp
+time ./a.out
+
+echo "***Execute insilico***"
+time ./insilico.out sp.dat nsets.conf ssets.conf 
+
+echo "***Plot data***"
+time gnuplot "plot"
+
+echo "***Done***"

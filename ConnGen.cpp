1be0886cf21170cd7c2b396cd0b439077d172b9a
himanshu <himanshu14121992@gmail.com>
2015-03-16 19:04:52 +0530

    changes in ssets.conf construct

--- ConnGen.cpp
+++ ConnGen.cpp
@@ -36,7 +36,7 @@ void conn_Syn(int G, int N)
         for(j=i;j<i+N;j++)
             for(k=i+N;k<i+N+N;k++)
             {    
-                s_sets<<"dxdt:2,g1:0,g2:0,gsyn:1.9,delay:0,esyn:0,tau_psp:1.7,pre:";
+                s_sets<<"dxdt:2,g1:0,g2:0,thresh:-55,gsyn:1.9,delay:0,esyn:0,tau_psp:1.7,pre:";
                 s_sets<<j<<",post:"<<k<<";"<<"\n";
             }
     s_sets.close();
@@ -49,15 +49,15 @@ void conn_Neu(int G, int N)
     for(int i=0;i<N*G;i++)
         if(i<N)	
         {
-            n_sets<<"dxdt:2,v:"<<normal(-63,5)<<",gk:0,v_reset:-70,v_rest:-70,tau_ref:1,tau_rel:15,syn_weight:1,noise_weight:12";
-            n_sets<<",gbar_k:8,e_k:-65,v_th:"<<-55<<",t_rest:"<<0<<",spike:0,iext:"<<140;
+            n_sets<<"dxdt:2,v:"<<-55.6<<",gk:0,v_reset:-70,last_spike:-1000,v_rest:-70,tau_ref:1,tau_rel:15,syn_weight:1,noise_weight:1";
+            n_sets<<",gbar_k:8,e_k:-65,v_th:"<<-55<<",t_rest:"<<0<<",spike:-55.6,iext:"<<140;
             n_sets<<",tau_m:"<<10;
-            n_sets<<",pulsewidth:"<<20<<";"<<"\n";   
+            n_sets<<",pulsewidth:"<<10<<";"<<"\n";   
         }
         else
         {
-            n_sets<<"dxdt:2,v:"<<normal(-63,5)<<",gk:0,v_reset:-70,v_rest:-70,tau_ref:1,tau_rel:15,syn_weight:1,noise_weight:12";
-            n_sets<<",gbar_k:8,e_k:-65,v_th:"<<-55<<",t_rest:"<<0<<",spike:0,iext:"<< 0;
+            n_sets<<"dxdt:2,v:"<<-56.6<<",gk:0,v_reset:-70,last_spike:-1000,v_rest:-70,tau_ref:1,tau_rel:15,syn_weight:1,noise_weight:1";
+            n_sets<<",gbar_k:8,e_k:-65,v_th:"<<-55<<",t_rest:"<<0<<",spike:-56.6,iext:"<< 0;
             n_sets<<",tau_m:"<<10;
             n_sets<<",pulsewidth:"<<225<<";"<<"\n";   
         } 
